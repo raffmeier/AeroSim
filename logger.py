Fix rotation matrix in NSL update, implement basic IMU model

logger.py
@@ -1,6 +1,7 @@
 import numpy as np
 from quad import *
 from controller.pid import *
+from sensor.imu import IMU
 from utils import *
 import pandas as pd
 
@@ -18,6 +19,7 @@ class Logger():
         self.eulerangles = np.zeros((3, timesteps))
         self.motor_thrust = np.zeros((4, timesteps))
         self.total_thrust = np.zeros(timesteps)
+        self.accel = np.zeros((3, timesteps))
 
         #Controller
         self.angle_setpoint = np.zeros((3, timesteps))
@@ -25,7 +27,11 @@ class Logger():
         self.integrated_error = np.zeros((3, timesteps))
         self.motor_commands = np.zeros((4, timesteps))
 
-    def log(self, quad: QuadcopterDynamcis, ctrl: AttitudePID, step, dt):
+        #IMU
+        self.imu_accel = np.zeros((3, timesteps))
+        self.imu_rates = np.zeros((3, timesteps))
+
+    def log(self, quad: QuadcopterDynamcis, ctrl: AttitudePID, imu: IMU, step, dt):
         self.pos[:, step] = quad.pos
         self.vel[:, step] = quad.vel
         self.q[:, step] = quad.q
@@ -36,11 +42,15 @@ class Logger():
         self.eulerangles[:, step] = quat_to_euler(quad.q) * 180 / np.pi
         self.motor_thrust[:, step] = quad.params.kT * np.square(quad.n)
         self.total_thrust[step] = np.sum(self.motor_thrust)
+        self.accel[:, step] = quad.accel
 
         self.angle_setpoint[:, step] = np.array([ctrl.roll_pid.setpoint, ctrl.pitch_pid.setpoint, ctrl.yaw_pid.setpoint])
         self.angle_error[:, step] = np.array([ctrl.roll_pid.err, ctrl.pitch_pid.err, ctrl.yaw_pid.err])
         self.integrated_error[:, step] = np.array([ctrl.roll_pid.integrated_err, ctrl.pitch_pid.integrated_err, ctrl.yaw_pid.integrated_err])
         self.motor_commands[:, step] = np.array([ctrl.motor_commands])
+
+        self.imu_accel[:, step] = imu.meas_accel
+        self.imu_rates[:, step] = imu.meas_rates * 180 / np.pi
 
     def save_csv(self, filename):
         df = pd.DataFrame({
@@ -74,6 +84,9 @@ class Logger():
             "roll_integrated_error": self.integrated_error[0, :],
             "pitch_integrated_error": self.integrated_error[1, :],
             "yaw_integrated_error": self.integrated_error[2, :],
+            "accel_x": self.accel[0, :],
+            "accel_y": self.accel[1, :],
+            "accel_z": self.accel[2, :],
         })
 
         df.to_csv(filename, index=False)

quad.py
@@ -20,11 +20,16 @@ class QuadcopterParam():
 class QuadcopterDynamcis():
     def __init__(self, params: QuadcopterParam):
         self.params = params
+        
+        #State
         self.pos = np.zeros(3)
         self.vel = np.zeros(3)
         self.q = np.array([1, 0, 0, 0])
         self.w = np.zeros(3)
         self.n = np.full(4, 1136)
+
+        #Additional
+        self.accel = np.zeros(3)
     
     def step(self, ncmd, dt):
 
@@ -58,7 +63,7 @@ class QuadcopterDynamcis():
 
             Fg = np.array([0, 0, -9.81 * self.params.mass]) #Gravitational force in world frame
 
-            state_dot[3:6] = 1/self.params.mass * (R @ (-T_total + Fd) + Fg) #NSL in world frame
+            state_dot[3:6] = 1/self.params.mass * (R.T @ (-T_total + Fd) + Fg) #NSL in world frame
 
             state_dot[6:10] = 0.5 * omega_matrix(w) @ q #Quaternion dynamics
         
@@ -99,3 +104,6 @@ class QuadcopterDynamcis():
         self.q = S[6:10]
         self.w = S[10:13]
         self.n = np.clip(S[13:17], 0, self.params.nmax)
+
+        state_dot_final = state_dynamics(S, ncmd)
+        self.accel = state_dot_final[3:6]

sim.py
@@ -4,17 +4,21 @@ from quad import *
 from logger import *
 from plot import *
 from controller.pid import *
+from sensor.imu import *
 import time
 
 if __name__ == '__main__':
 
     dt = 0.01
-    t_sim = 15
+    t_sim = 20
     timesteps = int(t_sim/dt)
 
     QuadParams = QuadcopterParam()
     Quad = QuadcopterDynamcis(QuadParams)
     Log = Logger(timesteps)
+
+    IMUP = IMUParam()
+    Imu= IMU(IMUP, dt)
 
     Controller = AttitudePID(dt)
 
@@ -22,10 +26,10 @@ if __name__ == '__main__':
 
     for step in range(timesteps):
 
-        angle_setpoint = np.array([0, 0, np.deg2rad(90) * np.sin(step/500)])
+        angle_setpoint = np.array([0, 0, 0])
 
         if(step > 400 and step < 800):
-            angle_setpoint = np.array([0.2, 0.35, np.deg2rad(90) * np.sin(step/500)])
+            angle_setpoint = np.array([0.0, 0.35, 0])
 
 
         input = Controller.update(angle_setpoint, quat_to_euler(Quad.q), np.zeros(3), Quad.params.mass * 9.81)
@@ -34,10 +38,11 @@ if __name__ == '__main__':
 
         Quad.step(input, dt)
 
-        Log.log(Quad, Controller, step, dt)
+        Imu.step(Quad.accel, Quad.w, quat_to_R(Quad.q))
+
+        Log.log(Quad, Controller, Imu, step, dt)
     
     print("Finished simulation in " + str(np.round(time.time() - start_time, 2)) + "s")
-    
     plot(Log)
     Log.save_csv('output/log.csv')
 